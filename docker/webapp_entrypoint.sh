aebf6d87d70ef22e147574c35ce8d617f0f23105
Maciej 'Kasprzol' Kasprzyk <git_blame@kasprzyk.tech>
2022-05-07 22:52:56 +0200

    Renamed directory

--- docker/webapp_entrypoint.sh
+++ docker/webapp_entrypoint.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 
 # In production this would be at least a number of uwsgi/gunicorn processes
 # behind a proxy like nginx or haproxy.
-poetry run gunicorn -b 0.0.0.0:8000 -w 4 coins.wsgi:application
+poetry run gunicorn -b 0.0.0.0:8000 -w 4 django_project.wsgi:application
